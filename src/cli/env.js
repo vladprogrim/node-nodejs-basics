d18fe56babc314207fe7dfcdc56753c91198ea9d
vladprogrim <113541340+vladprogrim@users.noreply.github.com>
2025-04-28 15:47:11 +0300

    docs: completed next 2 tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const onlyRss = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+  console.log(onlyRss);
 };
 
 parseEnv();
